7ac5f9deddb704275ae5b69165ce181b65a8e7d0
Bryan Yehuda <bryanyehuda@gmail.com>
2021-06-22 20:40:44 +0700

    Initial Configuration Run

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,8 +3,8 @@ const path = require('path');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
-  mode: 'development',
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-  },
+    mode: 'development',
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+    },
 });

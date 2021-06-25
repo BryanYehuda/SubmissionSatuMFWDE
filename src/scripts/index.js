26e50be2fcd11534cf827d4159f0a2d1c1d0dab3
Bryan Yehuda <bryanyehuda@gmail.com>
2021-06-26 00:49:02 +0700

    Added Off Canvas Menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,4 +2,26 @@ import 'regenerator-runtime';
 import '../styles/main.css';
 import '../styles/queries.css';
 
-console.log('Hello Coders! :)');
+const menu = document.querySelector('#menu');
+const head = document.querySelector('header');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function(event) {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+    var element = document.getElementById("wrapperid");
+    element.classList.remove("wrapper");
+});
+
+head.addEventListener('click', function() {
+    drawer.classList.remove('open');
+    var element = document.getElementById("wrapperid");
+    element.classList.add("wrapper");
+});
+
+main.addEventListener('click', function() {
+    drawer.classList.remove('open');
+    var element = document.getElementById("wrapperid");
+    element.classList.add("wrapper");
+});

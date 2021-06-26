73ba0a4f47a02f9bf7544ebd05fb392f124ccbab
Bryan Yehuda <bryanyehuda@gmail.com>
2021-06-26 11:15:25 +0700

    Update to Off-Canvas Menu

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,21 +7,34 @@ const head = document.querySelector('header');
 const main = document.querySelector('main');
 const drawer = document.querySelector('#drawer');
 
+const addWrapper = function() {
+    if (window.screen.width >= 500) {
+        var element = document.getElementById("wrapperid");
+        element.classList.add("wrapper");
+    }
+}
+
+const removeWrapper = function() {
+    if (window.screen.width < 500) {
+        var element = document.getElementById("wrapperid");
+        element.classList.remove("wrapper");
+    }
+}
+
+window.addEventListener("resize", addWrapper);
+window.addEventListener("onload", addWrapper);
+window.addEventListener("resize", removeWrapper);
+window.addEventListener("onload", removeWrapper);
+
 menu.addEventListener('click', function(event) {
     drawer.classList.toggle('open');
     event.stopPropagation();
-    var element = document.getElementById("wrapperid");
-    element.classList.remove("wrapper");
 });
 
 head.addEventListener('click', function() {
     drawer.classList.remove('open');
-    var element = document.getElementById("wrapperid");
-    element.classList.add("wrapper");
 });
 
 main.addEventListener('click', function() {
     drawer.classList.remove('open');
-    var element = document.getElementById("wrapperid");
-    element.classList.add("wrapper");
 });
